35a08c41d2d94c79e1316fa063c8c782617619e6
Amin Ghazi <aminghazi@outlook.com>
2020-08-27 18:32:49 +0430

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello Auth Next</h1>
+            </>
+        )
+    }
+}
+
+export default Home;
